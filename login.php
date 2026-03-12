725a9ee1d0a77c95a71fa144f8da47d3040bfd64
Marechal Mael <124532665+nonomama25@users.noreply.github.com>
2026-03-12 15:11:12 +0100

    normalement login finit 7

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ $message = "";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $prenom = $_POST['identifier'];
-    $mdp = $_POST['password'];}
+    $mdp = $_POST['password'];
 
     $sql = "SELECT * FROM utilisateur WHERE prenom = :prenom AND mot_de_passe = :mdp";
     $stmt = $route->prepare($sql);
@@ -35,7 +35,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             header("Location: eleve.php");
         }
         exit;
+    } else {
+        $message = "<div class='alert alert-danger text-center'>Identifiant ou mot de passe incorrect</div>";
     }
+}
 
 ?>
 
