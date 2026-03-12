55a6fb55f8d0dc01aad47e530200e07c7d0051c6
Marechal Mael <124532665+nonomama25@users.noreply.github.com>
2026-03-12 14:35:33 +0100

    Login/css

--- login.php
+++ login.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="style.css">
     <title>LOGIN</title>
 </head>
 <body>
